1a57e70a493441e25d8cfd62bf69856e053ae802
Leonardo Jimenez <ljimenez@stancedata.com>
2015-03-18 23:22:28 -0400

    fix identation on base.py file

--- app/settings/base.py
+++ app/settings/base.py
@@ -78,28 +78,28 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-    #TEMPLATE CONFIGURATION
-    # See: https://docs.djangoproject.com/en/dev/ref/settings/#template-context-processors
-    TEMPLATE_CONTEXT_PROCESSORS = (
-        'django.contrib.auth.context_processors.auth',
-        'allauth.account.context_processors.account',
-        'allauth.socialaccount.context_processors.socialaccount',
-        'django.core.context_processors.debug',
-        'django.core.context_processors.i18n',
-        'django.core.context_processors.media',
-        'django.core.context_processors.static',
-        'django.core.context_processors.tz',
-        'django.contrib.messages.context_processors.messages',
-        'django.core.context_processors.request',
-        # Your stuff: custom template context processers go here
-    )
+#TEMPLATE CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#template-context-processors
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'allauth.account.context_processors.account',
+    'allauth.socialaccount.context_processors.socialaccount',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request',
+    # Your stuff: custom template context processers go here
+)
 
-    # See: https://docs.djangoproject.com/en/dev/ref/settings/#template-dirs
-    TEMPLATE_DIRS = (
-        join(BASE_DIR, 'templates'),
-    )
+ # See: https://docs.djangoproject.com/en/dev/ref/settings/#template-dirs
+TEMPLATE_DIRS = (
+    join(BASE_DIR, 'templates'),
+)
 
-    TEMPLATE_LOADERS = (
-        'django.template.loaders.filesystem.Loader',
-        'django.template.loaders.app_directories.Loader',
-    )
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
