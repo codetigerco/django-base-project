1f45200dc0c88af6bf42df8ddb9bfbf1ee3451c8
Leonardo Jimenez <ljimenez@stancedata.com>
2015-03-27 01:34:14 -0400

    create a basic User model

---
+++ app/apps/users/forms.py
@@ -0,0 +1,2 @@
+"""Basic User Forms"""
+# to implement something like https://github.com/jcugat/django-custom-user/blob/master/custom_user/forms.py
---
+++ app/apps/users/models.py
@@ -0,0 +1,97 @@
+"""
+This model contains the basic definition of an user.
+
+We made the following assumptions:
+    - The user will login with his/her email on all instances of the website.
+    - We prefer to extend what django already has than to create something new.
+"""
+#Import the AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
+#import the basic Django ORM models library
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
+
+
+class UserManager(BaseUserManager):
+    """Custom manager for user"""
+
+    def _create_user(self, email, password,
+                     is_staff, is_superuser, **extra_fields):
+        """ Create and save an User with the given email and password.
+        :param str email: user email
+        :param str password: user password
+        :param bool is_staff: whether user staff or not
+        :param bool is_superuser: whether user admin or not
+        :return custom_user.models.User user: user
+        :raise ValueError: email is not set
+        """
+        now = timezone.now()
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        is_active = extra_fields.pop("is_active", True)
+        user = self.model(email=email, is_staff=is_staff, is_active=is_active,
+                          is_superuser=is_superuser, last_login=now,
+                          date_joined=now, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """ Create and save an User with the given email and password.
+        :param str email: user email
+        :param str password: user password
+        :return custom_user.models.User user: regular user
+        """
+        is_staff = extra_fields.pop("is_staff", False)
+        return self._create_user(email, password, is_staff, False,
+                                 **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """ Create and save an User with the given email and password.
+        :param str email: user email
+        :param str password: user password
+        :return custom_user.models.User user: admin user
+        """
+        return self._create_user(email, password, True, True,
+                                 **extra_fields)
+
+
+class User(AbstractBaseUser):
+    """
+    User with the same behaviour as Django's default User.
+    User does not have username field. Uses email as the
+    USERNAME_FIELD for authentication.
+    Inherits from both the AbstractBaseUser.
+    The following attributes are inherited from the superclasses:
+        * password
+        * last_login
+        * is_superuser
+    """
+    email = models.EmailField(max_length=True, unique=True, db_index=True)
+    is_staff = models.BooleanField(
+        _('staff status'), default=False, help_text=_(
+            'Designates whether the user can log into this admin site.'))
+    is_active = models.BooleanField(_('active'), default=True, help_text=_(
+        'Designates whether this user should be treated as '
+        'active. Unselect this instead of deleting accounts.'))
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __unicode__(self):
+        return self.email
+
+    def get_full_name(self):
+        """ Return the email."""
+        return self.email
+
+    def get_short_name(self):
+        """ Return the email."""
+        return self.email
+
+    def save(self, *args, **kwargs):
+        super(User, self).save(*args, **kwargs)
+
