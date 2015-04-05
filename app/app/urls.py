c0011a6bed7773e9cad5d489a2186f2b563e9da8
Leonardo Jimenez <ljimenez@stancedata.com>
2015-04-05 15:53:41 -0400

    load media files locally while DEBUG=True

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
@@ -8,3 +9,9 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT}))
